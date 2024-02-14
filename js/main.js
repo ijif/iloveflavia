4fc2c3269eb69881aa2c2a7583e92c7300c444cf
ijif <ijif@protonmail.ch>
2024-02-14 04:02:54 -0500

    adding animated heart

--- js/main.js
+++ js/main.js
@@ -1,76 +1,129 @@
-const SPRITE_WIDTH = 13;
-const SPRITE_HEIGHT = 14;
-const BORDER_WIDTH = 1;
-const SPACING_WIDTH = 1;
+var $window = $(window), gardenCtx, gardenCanvas, $garden, garden;
+var clientWidth = $(window).width();
+var clientHeight = $(window).height();
+$(function () {
+	$loveHeart = $("#loveHeart");
+	var a = $loveHeart.width() / 2;
+	var b = $loveHeart.height() / 2 - 55;
+	$garden = $("#garden");
+	gardenCanvas = $garden[0];
+	gardenCanvas.width = $("#loveHeart").width();
+	gardenCanvas.height = $("#loveHeart").height();
+	gardenCtx = gardenCanvas.getContext("2d");
+	gardenCtx.globalCompositeOperation = "lighter";
+	garden = new Garden(gardenCtx, gardenCanvas);
+	$("#content").css("width", $loveHeart.width() + $("#code").width());
+	$("#content").css("height", Math.max($loveHeart.height(), $("#code").height()));
+	$("#content").css("margin-top", Math.max(($window.height() - $("#content").height()) / 2 - 50, 10));
+	$("#content").css("margin-left", Math.max(($window.width() - $("#content").width()) / 2, 10));
+	setInterval(function () {
+		garden.render()
+	}, Garden.options.growSpeed)
+});
+$(window).resize(function () {
+	var b = $(window).width();
+	var a = $(window).height();
+	if (b != clientWidth && a != clientHeight) {
+		location.replace(location)
+	}
+});
 
-function spritePositionToImagePosition(row, col) {
-    return {
-        x: (
-            BORDER_WIDTH +
-            col * (SPACING_WIDTH + SPRITE_WIDTH)
-        ),
-        y: (
-            BORDER_WIDTH +
-            row * (SPACING_WIDTH + SPRITE_HEIGHT)
-        )
-    }
+function getHeartPoint(c) {
+	var b = c / Math.PI;
+	var a = 19.5 * (16 * Math.pow(Math.sin(b), 3));
+	var d = -20 * (13 * Math.cos(b) - 5 * Math.cos(2 * b) - 2 * Math.cos(3 * b) - Math.cos(4 * b));
+	return new Array(offsetX + a, offsetY + d)
 }
 
-var canvas = document
-            .querySelector('canvas');
-var context = canvas
-              .getContext('2d');
-
-var spriteSheetLocation = 'js/img/squirrel.png';
-var image = new Image();
-image.src = spriteSheetLocation;
-image.crossOrigin = true;
-
-// extract all of our frames
-var karelright0 = spritePositionToImagePosition(0, 0);
-var karelright1 = spritePositionToImagePosition(0, 1);
-var karelright2 = spritePositionToImagePosition(0, 2);
-var karelleftt0 = spritePositionToImagePosition(1, 0);
-var karelleft1 = spritePositionToImagePosition(1, 1);
-var karelleft2 = spritePositionToImagePosition(1, 2);
-
-var walkCycle = [
-    karelright0,
-    karelright1,
-    karelright2,
-    karelright1
-];
-
-var frameIndex = 0;
-var frame;
-function animate() {
-    // once we hit the end of the cycle,
-    // start again
-    if (frameIndex === walkCycle.length) {
-        frameIndex = 0;
-    }
-    frame = walkCycle[frameIndex];
-    context.clearRect(
-        0,
-        0,
-        canvas.width,
-        canvas.height
-    );
-    context.drawImage(
-        image,
-        frame.x,
-        frame.y,
-        SPRITE_WIDTH,
-        SPRITE_HEIGHT,
-        0,
-        0,
-        SPRITE_WIDTH,
-        SPRITE_HEIGHT
-    );
-    frameIndex += 1;
+function startHeartAnimation() {
+	var c = 50;
+	var d = 10;
+	var b = new Array();
+	var a = setInterval(function () {
+		var h = getHeartPoint(d);
+		var e = true;
+		for (var f = 0; f < b.length; f++) {
+			var g = b[f];
+			var j = Math.sqrt(Math.pow(g[0] - h[0], 2) + Math.pow(g[1] - h[1], 2));
+			if (j < Garden.options.bloomRadius.max * 1.3) {
+				e = false;
+				break
+			}
+		}
+		if (e) {
+			b.push(h);
+			garden.createRandomBloom(h[0], h[1])
+		}
+		if (d >= 30) {
+			clearInterval(a);
+			showMessages()
+		} else {
+			d += 0.2
+		}
+	}, c)
 }
 
-image.onload = function() {
-    setInterval(animate, 250);
+(function (a) {
+	a.fn.typewriter = function () {
+		this.each(function () {
+			var d = a(this), c = d.html(), b = 0;
+			d.html("");
+			var e = setInterval(function () {
+				var f = c.substr(b, 1);
+				if (f == "<") {
+					b = c.indexOf(">", b) + 1
+				} else {
+					b++
+				}
+				d.html(c.substring(0, b) + (b & 1 ? "_" : ""));
+				if (b >= c.length) {
+					clearInterval(e)
+				}
+			}, 75)
+		});
+		return this
+	}
+})(jQuery);
+
+function timeElapse(c) {
+	var e = Date();
+	var f = (Date.parse(e) - Date.parse(c)) / 1000;
+	var g = Math.floor(f / (3600 * 24));
+	f = f % (3600 * 24);
+	var b = Math.floor(f / 3600);
+	if (b < 10) {
+		b = "0" + b
+	}
+	f = f % 3600;
+	var d = Math.floor(f / 60);
+	if (d < 10) {
+		d = "0" + d
+	}
+	f = f % 60;
+	if (f < 10) {
+		f = "0" + f
+	}
+	var a = '<span class="digit">' + g + '</span> days <span class="digit">' + b + '</span> hours <span class="digit">' + d + '</span> minutes <span class="digit">' + f + "</span> seconds";
+	$("#elapseClock").html(a)
+}
+
+function showMessages() {
+	adjustWordsPosition();
+	$("#messages").fadeIn(5000, function () {
+		showLoveU()
+	})
+}
+
+function adjustWordsPosition() {
+	$("#words").css("position", "absolute");
+	$("#words").css("top", $("#garden").position().top + 195);
+	$("#words").css("left", $("#garden").position().left + 70)
+}
+
+function adjustCodePosition() {
+	$("#code").css("margin-top", ($("#garden").height() - $("#code").height()) / 2)
+}
+
+function showLoveU() {
+	$("#loveu").fadeIn(3000)
 };
-  
